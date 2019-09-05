29f7c8af527f563ee3c26f8c224ab46ded5544dc
AnshulAggarwal <anshul.aggarwal.sfd@gmail.com>
2019-09-05 16:55:35 +0530

    Optimized the problem and corrected the tagged value

--- maximum-size-sub-matrix-with-all-1s-in-a-binary-matrix.py
+++ maximum-size-sub-matrix-with-all-1s-in-a-binary-matrix.py
@@ -1,5 +1,5 @@
 """
-#102
+#54
 Maximum size square sub-matrix with all 1s
 Given a binary matrix, find out the maximum size square sub-matrix with all 1s.
 
@@ -22,26 +22,25 @@ maximum-size-square-sub-matrix-with-all-1s
 """
 
 import PrintMatrix as pm
+
+#S-Complexity: O(1) | T-Complexity: O(Row*Cols)
 def findSolution(matrix):
     rowIndex = len(matrix)-1
     colIndex = len(matrix[0])-1
-    maxCount = 0
+    maxSize = 1
     for i in range(rowIndex-1, -1, -1):
         for j in range(colIndex-1, -1, -1):
             if(matrix[i][j]>0):
                 right = matrix[i][j+1]
                 bottom = matrix[i+1][j]
                 bottomRight = matrix[i+1][j+1]
-                if(right>0 and bottom>0 and bottomRight>0):
-                    if(right == bottom and bottom == bottomRight):
-                        matrix[i][j] = right+1
-                    else:
-                        matrix[i][j] = max(right, bottomRight, bottom)
-                maxCount = max(maxCount, matrix[i][j])
+     
+                matrix[i][j] = min(right, bottomRight, bottom)+1
+                maxSize = max(maxSize, matrix[i][j])
     #pm.printMatrix(matrix)
-    return maxCount
-i
-f __name__ == "__main__":
+    return maxSize
+
+if __name__ == "__main__":
     matrix =[[0, 1, 1, 0, 1], 
     [1, 1, 0, 1, 0], 
     [0, 1, 1, 1, 0], 
