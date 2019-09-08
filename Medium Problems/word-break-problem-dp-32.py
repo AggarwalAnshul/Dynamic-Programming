c5bcc987ae1916028b3916f722157112797558f7
AnshulAggarwal <anshul.aggarwal.sfd@gmail.com>
2019-09-08 19:42:45 +0530

    Added word partitionning problem

---
+++ Medium Problems/word-break-problem-dp-32.py
@@ -0,0 +1,63 @@
+"""
+#M.16
+
+Word Break Problem | DP-32
+Given an input string and a dictionary of words, find out if the input string can be segmented into a space-separated sequence of dictionary words. See following examples for more details.
+This is a famous Google interview question, also being asked by many other companies now a days.
+
+Consider the following dictionary 
+{ i, like, sam, sung, samsung, mobile, ice, 
+  cream, icecream, man, go, mango}
+
+Input:  ilike
+Output: Yes 
+The string can be segmented as "i like".
+
+Input:  ilikesamsung
+Output: Yes
+The string can be segmented as "i like samsung" 
+or "i like sam sung".
+
+[+]Temporal marker            : 19:05 Hours,  | Sunday, Sept08, 19
+[+]Temporal marker untethered : 19:25 Hours,  | Sunday, Sept08, 19
+[+]Comments                   : Already had the apporach in my mind
+                                Implementation took 15 mins max
+                                All test cases passed
+                                Don't feel a dynamic solution is requried
+                                GFG Shows a DP Solution, However my solution has the same
+                                complexity as theirs, so i'm not moving into unchartered territory unnecessarily
+                                Consider the matter closed
+[+]Tread speed                : Relaxed
+[+]Level                      : Easy
+[+]LINK                       : https://www.geeksforgeeks.org/word-break-problem-dp-32/
+"""
+
+#T-Complexity: O(word*dic) | S-Complexity: O(n)
+def findSolution(word, start, end, dic):
+  
+    if(start==end and start==len(word)):
+        return True  #The case of success
+    if(end > (len(word)-1)):
+        return False #The case of failure
+    substring = word[start : end+1] #The substring to work on
+    for sub in dic:     #looking if this substring is in dic?
+        if(substring==sub):
+            print(sub)
+            return findSolution(word, end+1, end+1, dic) #starting afresh afterwards
+        
+    return findSolution(word, start, end+1, dic) #adding more char to substring
+
+if __name__ == "__main__":
+    #lis = [sum, dices, faces]
+    dic = ["mobile","samsung","sam","sung", "man","mango","icecream","and", 
+        	"go","i","like","ice","cream"]
+    word = "ilike"
+    word = "ilikesamsung"
+    word = "ilikeposamsung"
+    word = "iiiiiiii"
+    word = ""
+    word = "ilikelikeimangoiii"
+    word = "samsungandmango"
+    word = "samsungandmangok"
+    word = "ilikesamsungmobile"
+    print(findSolution(word, 0, 0, dic))
