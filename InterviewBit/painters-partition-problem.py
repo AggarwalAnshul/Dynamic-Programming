f002f3b8bb1f02e542bb24519b806d0583993ad1
AnshulAggarwal <anshul.aggarwal.sfd@gmail.com>
2020-02-04 09:55:12 +0530

    Added Painters Partition Problem

---
+++ InterviewBit/painters-partition-problem.py
@@ -0,0 +1,89 @@
+"""
+Given 2 integers A and B and an array of integars C of size N.
+
+Element C[i] represents length of ith board.
+
+You have to paint all N boards [C0, C1, C2, C3 … CN-1]. There are A painters available and each of them takes B units
+of time to paint 1 unit of board.
+
+Calculate and return minimum time required to paint all boards under the constraints that any painter will only paint
+contiguous sections of board.
+
+2 painters cannot share a board to paint. That is to say, a board
+cannot be painted partially by one painter, and partially by another.
+A painter will only paint contiguous boards. Which means a
+configuration where painter 1 paints board 1 and 3 but not 2 is
+invalid.
+Return the ans % 10000003
+
+
+
+Input Format
+
+The first argument given is the integer A.
+The second argument given is the integer B.
+The third argument given is the integer array C.
+Output Format
+
+Return minimum time required to paint all boards under the constraints that any painter will only paint contiguous
+sections of board % 10000003.
+Constraints
+
+1 <=A <= 1000
+1 <= B <= 10^6
+1 <= C.size() <= 10^5
+1 <= C[i] <= 10^6
+For Example
+
+Input 1:
+    A = 2
+    B = 5
+    C = [1, 10]
+Output 1:
+    50
+Explanation 1:
+    Possibility 1:- same painter paints both blocks, time taken = 55units
+    Possibility 2:- Painter 1 paints block 1, painter 2 paints block 2, time take = max(5, 50) = 50
+    There are no other distinct ways to paint boards.
+    ans = 50%10000003
+
+Input 2:
+    A = 10
+    B = 1
+    C = [1, 8, 11, 3]
+Output 2:
+    11
+
+[+]Temporal marker           : Tue, 9:47 | Feb 04, 20
+[+]Temporal marker untethered: Tue, 9:47 | Feb 04, 20
+[+]Comments                  : Took about a day or to come up with a DP solution with complexity O(k*N*N)
+                                * DP Solution devised on my own
+                                *for the more optimized binary solution would need help
+[+]Level                     : Medium Level
+[+]Tread Speed               : Relaxed
+[+]LINK                      : https://www.interviewbit.com/problems/painters-partition-problem
+
+"""
+import sys
+
+
+def paint(painters, time, board):
+    boards = len(board)
+    dp = [[0 for x in range(boards)] for y in range(painters)]
+    dp[0][0] = board[0]
+    for x in range(1, boards):
+        dp[0][x] = dp[0][x - 1] + board[x]
+
+    for i in range(1, painters):
+        for j in range(boards):
+            local_ans = sys.maxsize
+            for k in range(j + 1):
+                local_ans = min(local_ans, max(dp[i - 1][k], dp[0][j] - dp[0][k]))
+            dp[i][j] = local_ans
+    return (dp[painters - 1][boards - 1] * time) % 10000003
+
+
+if __name__ == '__main__':
+    # inputData = [painters, time, board[] ]
+    inputData = [3, 10, [640, 435, 647, 352, 8, 90, 960, 329, 859]]
+    print(paint(inputData[0], inputData[1], inputData[2]))
