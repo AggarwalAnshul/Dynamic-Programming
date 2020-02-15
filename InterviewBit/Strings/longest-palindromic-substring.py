f03462c25ec61ec5f6a7193eaabeb322a0c4c80b
AnshulAggarwal <anshul.aggarwal.sfd@gmail.com>
2020-02-15 11:28:47 +0530

    Optimized the solution | longest palindromic substring

--- InterviewBit/Strings/longest-palindromic-substring.py
+++ InterviewBit/Strings/longest-palindromic-substring.py
@@ -1,4 +1,20 @@
 """
+Given a string S, find the longest palindromic substring in S.
+
+Substring of string S:
+
+S[i...j] where 0 <= i <= j < len(S)
+
+Palindrome string:
+
+A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S.
+
+Incase of conflict, return the substring which occurs first ( with the least starting index ).
+
+Example :
+
+Input : "aaaabaaa"
+Output : "aaabaaa"
 
 [+]Temporal marker           : Wed, 23:21 | Feb 12, 20
 [+]Temporal marker untethered: Sat, 10:45 | Feb 15, 20
@@ -11,8 +27,12 @@
                                 *coded the solution at the very instant and the solution was fully correct
                                 *Matter is closed now
                                 *Might embark to reduce the space complexity to O(1)
-[+]Space Complexity          :O(N*N)
-[+]Time Complexity           :O(N*N)
+                                *Devised the O(1) Space complexity solution
+                                *Had help from GFG looked the approach and code
+                                *Wrote the code and got submitted in the first attempt
+                                *This approach worked like a CHARM!
+[+]Space Complexity          :O(N*N) | O(N*N)
+[+]Time Complexity           :O(1)  | O(N*N)
 [+]Level                     :MEDIUM
 [+]Tread Speed               :RELAXED
 [+]LINK                      : https://www.interviewbit.com/problems/longest-palindromic-substring
@@ -21,36 +41,71 @@
 """
 import math
 
-# Improving Solution with O(N) Space requirements
-    # T-complexity: O(N*N)
-    # S-complexity: O(N)
+
+# Trying an O(1) Space complexity Approach
+# Solution is up and rollin!
 def findSolution(string):
     length = len(string)
-    dp = [0 for x in range(length)]
-    dp[length-1] = 1
-    ans, maxi, maxj = 0, 0, 0
+    maxLength = 1
+    palindromestart = 0
 
-    print(dp)
-    for i in range(length-2, -1, -1):
-        for j in range(length-1, i-1, -1):
-            if i == j:
-                dp[j] = 1
-            else:
-                if string[i] == string[j]:
-                    if dp[j-1] != -1:
-                        dp[j] = dp[j-1] + 2
-                        if ans <= dp[j]:
-                            maxi, maxj = i, j
-                            ans = dp[j]
-                    else:
-                        dp[j] = -1
-                else:
-                    dp[j] = -1
-        print(dp)
-    print('ans: '+str(ans))
-    return string[maxi: maxj+1]
+    for i in range(0, length):
+        pivot = i
 
-#-----------Working Solution O(N*N) | O(N*N)
+        # finding for odd length palindromes
+        left = pivot - 1
+        right = pivot + 1
+        while left >= 0 and right < length and string[left] == string[right]:
+            if maxLength < right - left + 1:
+                maxLength = right - left + 1
+                palindromeStart = left
+            left -= 1
+            right += 1
+
+        # Looking for even length palindromes
+        left = i - 1
+        right = i
+        while left >= 0 and right < length and string[left] == string[right]:
+            if maxLength < right - left + 1:
+                maxLength = right - left + 1
+                palindromeStart = left
+            left -= 1
+            right += 1
+    print(maxLength)
+    return string[palindromeStart : palindromeStart + maxLength]
+
+
+# Improving Solution with O(N) Space requirements
+# T-complexity: O(N*N)
+# S-complexity: O(N)
+# def findSolution(string):
+#     length = len(string)
+#     dp = [0 for x in range(length)]
+#     dp[length - 1] = 1
+#     ans, maxi, maxj = 0, 0, 0
+#
+#     print(dp)
+#     for i in range(length - 2, -1, -1):
+#         for j in range(length - 1, i - 1, -1):
+#             if i == j:
+#                 dp[j] = 1
+#             else:
+#                 if string[i] == string[j]:
+#                     if dp[j - 1] != -1:
+#                         dp[j] = dp[j - 1] + 2
+#                         if ans <= dp[j]:
+#                             maxi, maxj = i, j
+#                             ans = dp[j]
+#                     else:
+#                         dp[j] = -1
+#                 else:
+#                     dp[j] = -1
+#         print(dp)
+#     print('ans: ' + str(ans))
+#     return string[maxi: maxj + 1]
+
+
+# -----------Working Solution O(N*N) | O(N*N)
 # def findSolution(string):
 #     length = len(string)
 #     # dp = [[0 for x in range(length)] for y in range(length)]
@@ -89,8 +144,10 @@ if __name__ == '__main__':
     string = "abacdfgacabd"
     string = "caccbcbaabacabaccacaaccaccaaccbbcbcbbaacabccbcccbbacbbacbccaccaacaccbbcc"
     string = "abacdfgacabd"
-    string = "aaaabaaa"
     string = "abacdgfdcaba"
-    string = "sibb"
     string = "abbcccbbbcaaccbababcbcabca"
+    string = 'forgeeksskeegfor'
+    string = "sibb"
+    string = "abacdgfdcaba"
+    string = "aaaabaaa"
     print(findSolution(string))
