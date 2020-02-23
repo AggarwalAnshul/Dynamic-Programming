b59531fa4d347df40a3b470f3a11f6425f177ccd
AnshulAggarwal <anshul.aggarwal.sfd@gmail.com>
2020-02-23 19:38:16 +0530

    solved one more problem

--- InterviewBit/Two Pointers/3-sum.py
+++ InterviewBit/Two Pointers/3-sum.py
@@ -1,41 +1,56 @@
 """
 
 [+]Temporal marker           : Sat, 18:23 | Feb 22, 20
-[+]Temporal marker untethered: Sat, 18:23 | Feb 22, 20
-[+]Comments                  :
-[+]Space Complexity          : O()
-[+]Time Complexity           : O()
-[+]Level                     :
-[+]Tread Speed               :
+[+]Temporal marker untethered: Sat, 16:26 | Feb 23, 20
+[+]Comments                  : Couldn't solve on my own with efficiency
+                               Implemented the solution approach form iB
+                               Matter is now closed
+[+]Space Complexity          : O(1)
+[+]Time Complexity           : O(N*N)
+[+]Level                     : MEDIUM
+[+]Tread Speed               : relaxed
 [+]LINK                      : https://www.interviewbit.com/problems/3-sum
 [+] Supplement Sources       : N/A
 """
 
 
 def findSolution(lis, target):
-    length = len(lis)
-    chosen = []
-    sum = 0
-    print("target: "+str(target))
-    for turn in range(0, 3):
-        closest = abs(target - lis[0])
-        temp = 0
-        for i in range(length):
-            if i not in chosen:
-                if closest > abs(target - lis[i]):
-                    temp = i
-                    closest = abs(target - lis[i])
-        chosen.append(temp)
-        print('chosen is: '+str(lis[temp]))
-        target -= lis[temp]
-        sum += lis[temp]
-    return sum
+    lis.sort()
+    length = len(lis) - 1
+    import sys
+    answer = sys.maxsize
+    print("sorted: "+str(lis))
+    for index in range(length):
+        print("index: " +str(index))
+        minimum = lis[index]
+        left = index + 1
+        right = length
+
+        while left < right:
+            print("\tleft: "+str(left)+" right: "+str(right))
+            if abs(target - lis[left] - lis[right] - minimum) < abs(target - answer):
+                answer = lis[left] + lis[right] + minimum
+                print("\tanswer is: " + str(answer))
+            if lis[left] + lis[right] + minimum > target:
+                right -= 1
+            elif lis[left] + lis[right] + minimum < target:
+                left += 1
+            else:
+                print("returning: "+str(lis[left] + lis[right] + minimum))
+                return lis[left] + lis[right] + minimum
+        print('best is: '+str(answer))
+    return answer
 
 
 if __name__ == "__main__":
     data = [[[-1, 4, 2, 1], 5],
             [[-5, 1, 4, -7, 10, -7, 0, 7, 3, 0, -2, -5, -3, -6, 4, -7, -8, 0, 4, 9, 4, 1, -8, -6, -6, 0, -9, 5, 3, -9,
               -5, -9, 6, 3, 8, -10, 1, -2, 2, 1, -9, 2, -3, 9, 9, -10, 0, -9, -2, 7, 0, -4, -3, 1, 6, -3], -1],
-            [[-5, 1, 4, -7, 10],1]]
-    for x in data:
+            [[-5, 1, 4, -7, 10], 1],
+            [[-3, 0, 0, 1, 2], -1],
+            [[-10, -10, -10], -5],
+            [[-4, -8, -10, -9, -1, 1, -2, 0, -8, -2], 0],
+            [[ 2, 1, -9, -7, -8, 2, -8, 2, 3, -8 ], -1]]
+    dataone= [[[ 2, 1, -9, -7, -8, 2, -8, 2, 3, -8 ], -1]]
+    for x in dataone:
         print("input: " + str(x) + " >> " + str(findSolution(x[0], x[1])))
