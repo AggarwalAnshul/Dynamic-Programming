205d397d389445562bf4e49ae081042a7902f279
Anshul Aggarwal <anshul.aggarwal.sfd.2@gmail.com>
2020-03-22 12:50:15 +0530

    Optimized > min-stack.py

--- InterviewBit/Stacks And Queues/min-stack.py
+++ InterviewBit/Stacks And Queues/min-stack.py
@@ -27,9 +27,12 @@ A: In this case, return -1
 
 [+]Temporal marker           : About 50 Hours Ago | Friday 20, 2020
 [+]Temporal marker untethered: 13:23 Hours | Sunday 22, 2020
-[+]Comments                  : Finally solved it, I don't think it was influenced by hint
-                                There is still room for improvement
-[+]Space Complexity          : O(2N)
+[+]Comments                  : Devised solution was accepted but not the most optimal
+                                >> Couldn't devise the most optimal of solution
+                                >> Editorial solution was an eye-opener
+                                >> Implemented the editorial solution & flashed it
+                                >> Matter is now officially closed
+[+]Space Complexity          : O(N+M) M is the number of min changes | Obsolete--> O(2N)
 [+]Time Complexity           : O(1)
 [+]Level                     : MEDIUM
 [+]Tread Speed               : Relaxed
@@ -44,7 +47,8 @@ A: In this case, return -1
 # if pushed element is greater than minimum element,do nothing to min stack
 # if popped element is equal to minimum element, pop the min stack
 # if popped element is greater than minimum element, do nothing to min stack
-class MinStack:
+
+class MinStack_Obsolete:
     # @param x, an integer
     stack, aux = [], []
 
@@ -68,6 +72,42 @@ class MinStack:
         if self.aux:
             return self.aux[-1]
         return -1
+
+
+# Accepted!
+# Implementation of editorial approach
+# Approach was to use a minimum variable to store the current minimum value
+# if pushed element < minimum, push the current minimum element along with new minimum element
+#   and update minimum
+# if popped element == minimum element, pop the two top element and make minimum the second popped element
+# variable minimum at any given point of time contains the current minimum value
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minimum = float("inf")
+
+
+    def push(self, x):
+        if x < self.minimum:
+            self.stack.append(self.minimum)
+            self.minimum = x
+        self.stack.append(x)
+
+    def pop(self):
+        if self.stack:
+            if self.stack.pop() == self.minimum:
+                self.minimum = self.stack.pop()
+
+    def getMin(self):
+        if self.stack:
+            return self.minimum
+        return -1
+
+    def top(self):
+        if self.stack:
+            return self.stack[-1]
+        return -1
+
 
 if __name__ == '__main__':
     stack = MinStack()
