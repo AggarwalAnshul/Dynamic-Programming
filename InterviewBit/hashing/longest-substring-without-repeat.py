debac59ea4e87c4f8308448b4aa218c3803f4605
AggarwalAnshul <whiskey0147>
2020-04-08 14:34:59 +0530

    Added >> fraction | Upper Medium

--- InterviewBit/hashing/longest-substring-without-repeat.py
+++ InterviewBit/hashing/longest-substring-without-repeat.py
@@ -18,11 +18,14 @@ Seen this question in a real interview beforeYesNo
 """
 [+]Temporal marker            :1449 Hours | Monday 06, 2020
 [+]Temporal marker Untethered :1554 Hours | Monday 06, 2020
-[+]Comments                   :
-[+]Space Complexity           : O()
-[+]Time Complexity            : O()
-[+]Level                      : 
-[+]Tread Speed                :
+[+]Comments                   : Too tired to optimize, will look tomorrow
+                                Flashed the editorial inspired solution
+                                editorial solution is essentially the same approach, the implementation is wildly consice and optimized
+                                Matter is officially closed now
+[+]Space Complexity           : O(N*N)
+[+]Time Complexity            : O(N*N)
+[+]Level                      :  MEDIUM
+[+]Tread Speed                : Relazed
 [+]LINK                      : https://www.interviewbit.com/problems/longest-substring-without-repeat.py
 [+] Supplement Sources       : N/A
 
@@ -57,7 +60,23 @@ def lengthOfLongestSubstring(string):
         final_length = current_length
         final_string = current_string
     return (final_length, final_string)
-
+    
+# ACCEPTED
+# EDITORIAL INSPIRED
+# FLASHED IT
+def lengthOfLongestSubstring_editorial_inspired(string):
+    hashmap, length = {}, len(string)
+    start, end = 0, 0
+    ans = 0
+    while start < length and end < length:
+        if string[end] in hashmap:
+            hashmap.pop(string[start])
+            start += 1
+        else:
+            ans = max(ans, end-start+1)
+            hashmap[string[end]] = end
+            end += 1
+    return max(ans, end-start)
 
 
 
@@ -68,6 +87,6 @@ if __name__ == "__main__":
         "dadbc",
         "Wnb9z9dMc7E8v1RTUaZPoDNIAXRlzkqLaa97KMWLzbitaCkRpiE4J4hJWhRcGnC8H6mwasgDfZ76VKdXhvEYmYrZY4Cfmf4HoSlchYWFEb1xllGKyEEmZOLPh1V6RuM7Mxd7xK72aNrWS4MEaUmgEn7L4rW3o14Nq9l2EN4HH6uJWljI8a5irvuODHY7A7ku4PJY2anSWnfJJE1w8p12Ks3oZRxAF3atqGBlzVQ0gltOwYmeynttUmQ4QBDLDrS4zn4VRZLosOITo4JlIqPD6t4NjhHThOjJxpMp9fICkrgJeGiDAwsb8a3I7Txz5BBKV9bEfMsKNhCuY3W0ZHqY0MhBfz1CbYCzwZZdM4p65ppP9s5QJcfjadmMMi26JKz0TVVwvNA8LP5Vi1QsxId4SI19jfcUH97wmZu0pbw1zFtyJ8GAp5yjjQTzFIboC1iRzklnOJzJld9TMaxqvBNBJKIyDjWrdfLOY8FGMOcPhfJ97Dph35zfxYyUf4DIqFi94lm9J0skYqGz9JT0kiAABQZDazZcNi80dSSdveSl6h3dJjHmlK8qHIlDsqFd5FMhlEirax8WA0v3NDPT8vPhwKpxcnVeu14Gcxr3h1wAXXV0y7Xy9qqB2NQ5HQLJ7cyXAckEYHsLCPSy28xcdNJatx1KLWohOQado4WywJbGvsFR17rKmvOPABweXnFD3odrbSMD4Na4nuBBswvMmFRTUOcf7jZi4z5JnJqXz6hitaPnaEtjoSEBq82a52nvqYy7hhldBoxen2et2OMadVEHeTYLL7GLsIhTP6UizHIuzcJMljo4lFgW5AyrfUlIBPAlhwaSiJtTvcbVZynDSM6RO1PqFKWKg2MHIgNhjuzENg2oFCfW7z5KJvEL9qWqKzZNc0o3BMRjS04NCHFvhtsteQoQRgz84XZBHBJRdekCdcVVXu9c01gYRAz7oIAxN3zKZb64EFKssfQ4HW971jv3H7x5E9dAszA0HrKTONyZDGYtHWt4QLhNsIs8mo4AIN7ecFKewyvGECAnaJpDn1MTTS4yTgZnm6N6qnmfjVt6ZU51F9BxH0jVG0kovTGSjTUkmb1mRTLQE5mTlVHcEz3yBOh4WiFFJjKJdi1HBIBaDL4r45HzaBvmYJPlWIomkqKEmQ4rLAbYG7C5rFfpMu8rHvjU7hP0JVvteGtaGn7mqeKsn7CgrJX1tb8t0ldaS3iUy8SEKAo5IZHNKOfEaij3nI4oRVzeVOZsH91pMsA4jRYgEohubPW8ciXwVrFi1qEWjvB8gfalyP60n1fHyjsiLW0T5uY1JzQWHKCbLVh7QFoJFAEV0L516XmzIo556yRH1vhPnceOCjebqgsmO78AQ8Ir2d4pHFFHAGB9lESn3OtJye1Lcyq9D6X93UakA3JKVKEt6JZDLVBMp4msOefkPKSw59Uix9d9kOQm8WCepJTangdNSOKaxblZDNJ5eHvEroYacBhd9UdafEitdF3nfStF7AhkSfQVC61YWWkKTNdx96OoJGTnxuqt4oFZNFtO7aMuN3IJAkw3m3kgZFRGyd3D3wweagNL9XlYtvZwejbjpkDOZz33C0jbEWaMEaUPw6BG49XqyQoUwtriguO0yvWyaJqD4ye3o0E46huKYAsdKAq6MLWMxF6tfyPVaoqOGd0eOBHbAF89XXmDd4AIkoFPXkAOW8hln5nXnIWP6RBbfEkPPbxoToMbV",
     ]
-    for index in range(3,4):
+    for index in range(len(test_cases)):
         test_case = test_cases[index]
         print("input: "+str(test_case)+"\n\tOUTPUT: :"+str(lengthOfLongestSubstring(test_case)))
